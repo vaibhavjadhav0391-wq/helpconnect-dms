687e07caf85e4cc080e5d7774352ffe04493edcf
Vaibhav Jadhav <vaibhavjadhav0391@gmail.com>
2026-04-09 21:15:38 +0530

    Configure Firebase and fix Google login integration

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC-csvDZFdAIfPNjnnVNB15vhCmwCbqrl4",
+  authDomain: "social-e66c5.firebaseapp.com",
+  projectId: "social-e66c5",
+  storageBucket: "social-e66c5.firebasestorage.app",
+  messagingSenderId: "767962531764",
+  appId: "1:767962531764:web:6921906a054d5b46b754e1"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export default app;
